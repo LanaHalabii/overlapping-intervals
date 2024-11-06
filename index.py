cb9daba4f5ebf94e7df8debfdfef16474dbb296a
LanaHalabi <LanaH6700@gmail.com>
2024-11-06 23:38:21 +0200

    Hello

---
+++ index.py
@@ -0,0 +1,8 @@
+over_inter = [[1,3], [2,4] , [6,8] ]
+
+#here, we need to write a code that sees if the
+#numbers that they counter is reading can form
+#an iteration of numbers (i+1), then these numbers
+#should be put in one array, with the smallest
+#number being the first item in the array,
+#and the largest number being the last name
